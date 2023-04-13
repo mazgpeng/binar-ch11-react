9926fe85e19653574df3a17df90b58b53cf51cab
raynato73@gmail.com <raynato73@gmail.com>
2023-04-13 21:45:27 +0700

    update fitur landingpage

--- src/pages/ContentHome.jsx
+++ src/pages/ContentHome.jsx
@@ -1,6 +1,7 @@
-import { Text, Container, Row, Button, Spacer, Card, Col } from "@nextui-org/react";
+import { Text, Container, Row, Button, Spacer, Image, Card, Col } from "@nextui-org/react";
 import { Box } from "./nextui/Box"
 import background from '../img/background.jpeg'
+import background1 from '../img/background1.jpg'
 
 export default function ContentHome() {
   return (
@@ -31,60 +32,55 @@ export default function ContentHome() {
       <Container xl css={{
         backgroundColor: "white",
       }}>
-        <Box css={{ px: "$12", mt: "$10", mb: "$10", mr: "$10", "@xsMax": { px: "$10" } }}>
-          <Row gap={2} >
-            <Col span={12} >
-              <Card css={{ $$cardColor: '$colors$secondary' }}>
-                <Card.Body>
-                  <Text h6 size={15} color="white" css={{ m: 0 }}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore magna aliqua. Purus gravida quis blandit turpis. Augue neque gravida in
-                    fermentum et sollicitudin ac orci. Et sollicitudin ac orci phasellus egestas. Elementum tempus
-                    egestas sed sed risus pretium quam vulputate. Interdum velit euismod in pellentesque massa
-                    placerat duis ultricies.
-                  </Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card css={{ $$cardColor: '$colors$primary' }}>
-                <Card.Body>
-                  <Text h6 size={15} color="white" css={{ m: 0 }}>
-                    2 of 3
-                  </Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card css={{ $$cardColor: '$colors$primary' }}>
-                <Card.Body>
-                  <Text h6 size={15} color="white" css={{ m: 0 }}>
-                    3 of 3
-                  </Text>
-                </Card.Body>
-              </Card>
-            </Col>
+        <Box css={{ px: "$12", mt: "$10", mb: "$10", "@xsMax": { px: "$10" } }}>
+
+          <Row gap={1.3} justify="center" align="center">
+            <Text css={{
+              textGradient: "45deg, $yellow600 -20%, $red600 50%",
+            }} size={50} > Featured Games</Text>
+            <Spacer y={1} />
+
           </Row>
         </Box>
-
       </Container>
 
       <Container xl css={{
-        backgroundColor: "gray",
+        backgroundImage: `url(${background1})`,
+        fontSize: '50px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '1000'
       }}>
-        <Row>
-          <Box css={{ px: "$12", mt: "$20", "@xsMax": { px: "$10" } }}>
-            <Text h1 css={{
-              textGradient: "45deg, $blue600 -20%, $pink600 50%",
-            }}> Massive Choice of Games !</Text>
-            <Text size={40} > Introducing GameStation</Text>
-            <Text size={30} > One Stop Platform to Cloud Gaming.</Text>
-            <Spacer y={0.5} />
-            <Button css={{ mb: "$10" }} shadow bordered color="gradient" auto>
-              Play Now!
-            </Button>
+        <Row justify="center" align="center">
+          <Box css={{ px: "$12", mt: "$20", mb: "$20", pl: "$15", "@xsMax": { px: "$10" } }}>
+            <Card css={{ $$cardColor: 'white' }}>
+              <Card.Body>
+                <Text h1>Rock Paper Scissors!</Text>
+                <Text size={20} > intransitive hand game, usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand. These shapes are "rock" (a closed fist), "paper" (a flat hand), and "scissors" (a fist with the index finger and middle finger extended, forming a V). The earliest form of "rock paper scissors"-style game originated in China and was subsequently imported into Japan, where it reached its modern standardized form, before being spread throughout the world in the early 20th century.</Text>
+                <Spacer y={0.5} />
+                <Button css={{ mb: "$10" }} shadow bordered color="gradient" auto>
+                  Play Now!
+                </Button>
+              </Card.Body>
+            </Card>
           </Box>
         </Row>
+      </Container>
+
+      <Container xl css={{
+        backgroundColor: "white",
+      }}>
+        <Box css={{ px: "$12", mt: "$10", mb: "$10", "@xsMax": { px: "$10" } }}>
+
+          <Row gap={1.3} justify="center" align="center">
+            <Text css={{
+              textGradient: "45deg, $yellow600 -20%, $red600 50%",
+            }} size={50} > Featured Games</Text>
+            <Spacer y={1} />
+
+          </Row>
+        </Box>
       </Container>
 
     </>
