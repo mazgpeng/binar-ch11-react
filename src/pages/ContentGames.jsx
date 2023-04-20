dbaef288cdf5e068acfb16e55fb7f159ec795135
M FERI MAULANA MUTTAQIN <mazgpeng@gmail.com>
2023-04-20 19:34:07 +0700

    fix style

--- src/pages/ContentGames.jsx
+++ src/pages/ContentGames.jsx
@@ -11,48 +11,50 @@ export default function ContentGameList() {
   return (
     <div className="latar">
         <div className="container">
-            <div className="p-4">
-            <h1>Game List <Badge bg="info">BARU</Badge> </h1>
-                <div className="d-flex justify-content-around">
-                <Card style={{ width: '15rem' }}>
-                    <Card.Img variant="top" src={ suit } />
-                    <Card.Body>
-                    <Card.Title>Batu Gunting Kertas</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Game Detail</Button>
-                    </Card.Body>
-                </Card>
+            <div className="formgmlist">
+                <div className="p-4">
+                <h1>Game List <Badge bg="info">BARU</Badge> </h1>
+                    <div className="d-flex justify-content-around">
+                        <Card style={{ width: '15rem' }}>
+                            <Card.Img variant="top" src={ suit } />
+                            <Card.Body>
+                            <Card.Title>Batu Gunting Kertas</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Game Detail</Button>
+                            </Card.Body>
+                        </Card>
 
-                <Card style={{ width: '15rem' }}>
-                    <Card.Img variant="top" src= {coming1} />
-                    <Card.Body>
-                    <Card.Title>-</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="secondary">Segera Hadir</Button>
-                    </Card.Body>
-                </Card>
+                        <Card style={{ width: '15rem' }}>
+                            <Card.Img variant="top" src= {coming1} />
+                            <Card.Body>
+                            <Card.Title>-</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="secondary">Segera Hadir</Button>
+                            </Card.Body>
+                        </Card>
 
-                <Card style={{ width: '15rem' }}>
-                    <Card.Img variant="top" src={ coming2 } />
-                    <Card.Body>
-                    <Card.Title>-</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="secondary">Segera Hadir</Button>
-                    </Card.Body>
-                </Card>
+                        <Card style={{ width: '15rem' }}>
+                            <Card.Img variant="top" src={ coming2 } />
+                            <Card.Body>
+                            <Card.Title>-</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="secondary">Segera Hadir</Button>
+                            </Card.Body>
+                        </Card>
 
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
         <Footer />
     </div>
   );
