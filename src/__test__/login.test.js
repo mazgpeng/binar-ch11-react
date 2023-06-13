ffed70ee2b9313f7717e605fcb08ed0f6d542bf6
M FERI MAULANA MUTTAQIN <mazgpeng@gmail.com>
2023-06-13 11:04:41 +0700

    test

--- src/__test__/login.test.js
+++ src/__test__/login.test.js
@@ -1,56 +1,41 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
 import { ContentLogin } from '../pages/ContentLogin';
 
-jest.mock('react-router', () => ({
-  useNavigate: jest.fn()
-}));
-
-describe('ContentLogin', () => {
-  beforeEach(() => {
+describe('ContentLogin component', () => {
+  test('renders login component', () => {
     render(<ContentLogin />);
+    const loginText = screen.getByText('log in');
+    expect(loginText).toBeInTheDocument();
   });
 
-  test('renders login form', () => {});
+  test('displays error message when email is not provided', () => {
+    render(<ContentLogin />);
+    fireEvent.click(screen.getByText('Login'));
+    const errorText = screen.getByText('Email is required');
+    expect(errorText).toBeInTheDocument();
+  });
 
-  test('handles input change', () => {});
-
-  test('handles login button click', async () => {
-    const emailInput = screen.getByLabelText('Email');
-    const passwordInput = screen.getByLabelText('Password');
-    const loginButton = screen.getByText('Login');
-
-    const mockNavigate = jest.fn();
-    const mockSignInWithEmailAndPassword = jest.fn(() =>
-      Promise.resolve({ user: { accessToken: 'testAccessToken' } })
-    );
-
-    jest.mock('../service/firebase', () => ({
-      getAuth: jest.fn(() => ({
-        signInWithEmailAndPassword: mockSignInWithEmailAndPassword,
-        languageCode: ''
-      }))
-    }));
-
-    const { useNavigate } = require('react-router');
-    useNavigate.mockReturnValue(mockNavigate);
-
-    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: 'password123' } });
-
-    await act(async () => {
-      fireEvent.click(loginButton);
+  test('displays error message when password is not provided', () => {
+    render(<ContentLogin />);
+    fireEvent.change(screen.getByPlaceholderText('Email'), {
+      target: { value: 'test@example.com' }
     });
+    fireEvent.click(screen.getByText('Login'));
+    const errorText = screen.getByText('Password is required');
+    expect(errorText).toBeInTheDocument();
+  });
 
-    expect(mockSignInWithEmailAndPassword).toHaveBeenCalledWith(
-      expect.any(Object),
-      'test@example.com',
-      'password123'
-    );
+  test('performs login with valid email and password', () => {
+    render(<ContentLogin />);
+    fireEvent.change(screen.getByPlaceholderText('Email'), {
+      target: { value: 'test@example.com' }
+    });
+    fireEvent.change(screen.getByPlaceholderText('Password'), { target: { value: 'password123' } });
+    fireEvent.click(screen.getByText('Login'));
+  });
 
-    await screen.findByText('login successfully');
-
-    expect(mockNavigate).toHaveBeenCalledWith('/home');
-    expect(mockNavigate).toHaveBeenCalledTimes(1);
+  test('performs login with Google', () => {
+    render(<ContentLogin />);
+    fireEvent.click(screen.getByRole('button', { name: 'Login with Google' }));
   });
 });
